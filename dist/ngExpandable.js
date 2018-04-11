c57f81d401a39c84441c92aa4c60c81bfcabe408
Matt Brewerton <mattbrewerton@outlook.com>
2018-04-11 12:21:34 +0100

    Enable smooth transitions with dyamic sizing elements

--- dist/ngExpandable.js
+++ dist/ngExpandable.js
@@ -1,53 +1,62 @@
 angular
-    .module('mbrewerton.ngExpandable', ['ngAnimate'])
-    .directive('expandable', function () {
-        var directiveController =
-            ['$scope', '$element', '$attrs', '$timeout',
-                function directiveController($scope, $element, $attrs, $timeout) {
-                    $timeout(function () {
-                        var el = $element[0];
+.module('mbrewerton.ngExpandable', ['ngAnimate'])
+.directive('expandable', function () {
+    var directiveController =
+        ['$scope', '$element', '$attrs', '$timeout',
+            function directiveController($scope, $element, $attrs, $timeout) {
+                $timeout(function () {
+                    var el = $element[0];
                         el.style.overflow = 'hidden';
-                        var maxHeight = el.scrollHeight;
-                        var expanded = true;
-                        var handleElement = document.getElementById($scope.handle);
+                    var _maxHeight = el.scrollHeight,
+                        _expanded = true,
+                        _handleElement = document.getElementById($scope.handle),
+                        _speed = $scope.speed || 0.5;
 
-                        var getTransition = function (speed, easing) {
-                            return (speed || 0.5) + 's height ' + (easing || 'ease-in-out');
-                        }
+                    var getTransition = function (easing) {
+                        return (_speed || 0.5) + 's height ' + (easing || 'ease-in-out');
+                    }
 
-                        var setHeight = function () {
-                            if (expanded) {
-                                el.style.height = maxHeight + 'px';
-                            } else {
-                                el.style.height = 0;
-                            }
-                        }
-                        el.style.transition = getTransition($scope.speed, $scope.easing);
-
-                        var expandFunction = function () {
-                            expanded = !expanded;
-                            setHeight();
-                        }
-
-                        if (handleElement) {
-                            // Apply the click event to our handle only
-                            handleElement.addEventListener('click', expandFunction);
+                    var setHeight = function () {
+                        _maxHeight = el.scrollHeight;
+                        if (_expanded) {
+                            el.style.height = _maxHeight + 'px';
+                            console.log(_speed);
+                            $timeout(function() {
+                                el.style.height = 'auto';
+                            }, _speed * 1000);
                         } else {
-                            // apply the expand to our element itself
-                            el.addEventListener('click', expandFunction);
+                            el.style.height = _maxHeight + 'px';
+                            $timeout(function() {
+                                el.style.height = 0;
+                            });
                         }
+                    }
+                    el.style.transition = getTransition($scope.easing);
 
+                    var expandFunction = function () {
+                        _expanded = !_expanded;
                         setHeight();
-                    });
-                }];
+                    }
 
-        return {
-            controller: directiveController,
-            restrict: 'A',
-            scope: {
-                handle: '@',
-                speed: '=?',
-                easing: '@?'
-            },
-        }
-    });
+                    if (_handleElement) {
+                        // Apply the click event to our handle only
+                        _handleElement.addEventListener('click', expandFunction);
+                    } else {
+                        // apply the expand to our element itself
+                        el.addEventListener('click', expandFunction);
+                    }
+
+                    setHeight();
+                });
+            }];
+
+    return {
+        controller: directiveController,
+        restrict: 'A',
+        scope: {
+            handle: '@',
+            speed: '=?',
+            easing: '@?'
+        },
+    }
+});
