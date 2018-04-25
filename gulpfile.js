9a32d7b41ad672bbdd0a3a945f2992fb56f7b1d7
Matt Brewerton <m.brewerton94@gmail.com>
2018-04-25 13:59:55 +0100

    Add basic default styling and transclusion.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp'),
-    minify = require('gulp-minify');
+    minify = require('gulp-minify'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch'),
     src = './src/',
     dist = './dist/';
 
-gulp.task('default', function() {
+gulp.task('default', function () {
+    Compile();
+});
+
+gulp.task('watch', function() {
+    watch(src + '*.*', function() { Compile(); })
+})
+
+function Compile() {
     gulp.src(src + 'ngExpandable.js')
         .pipe(gulp.dest(dist));
     gulp.src(src + 'ngExpandable.js')
@@ -11,4 +23,11 @@ gulp.task('default', function() {
             ext: '.min.js'
         }))
         .pipe(gulp.dest(dist));
-});
+
+    gulp.src(src + 'ngExpandable.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(dist))
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(dist));
+}
